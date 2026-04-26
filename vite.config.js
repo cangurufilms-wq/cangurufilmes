ac334de1ddf6f4a68417f0184217143bf3b18360
Canguru Filmes <pixel.agencia.contato@gmail.com>
2026-04-26 00:20:45 -0300

    Add Vite base configuration

--- vite.config.js
+++ vite.config.js
@@ -278,6 +278,7 @@ logger.error = (msg, options) => {
 }
 
 export default defineConfig({
+	base: '/',
 	customLogger: logger,
 	plugins: [
 		...(isDev ? [inlineEditPlugin(), editModeDevPlugin(), iframeRouteRestorationPlugin(), selectionModePlugin()] : []),
